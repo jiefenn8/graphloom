545f718b3b8bf7885385c0f4b71ebe3fd0779c2a
Javen Liu <jiefen8nz@gmail.com>
2019-04-04 21:17:11 +1300

    Add R2RML parser implementation

--- src/main/java/r2graph/r2rml/R2RMLParser.java
+++ src/main/java/r2graph/r2rml/R2RMLParser.java
@@ -1,5 +1,13 @@
 package r2graph.r2rml;
 
+import org.apache.jena.rdf.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import r2graph.configmap.ConfigMap;
+import r2graph.configmap.ConfigMapFactory;
+import r2graph.configmap.EntityMap;
+import r2graph.configmap.PredicateObjectMap;
+import r2graph.configmap.impl.PredicateObjectMapCom;
 import r2graph.util.MappingDocument;
 
 /**
@@ -11,15 +19,112 @@ import r2graph.util.MappingDocument;
  */
 public class R2RMLParser {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(R2RMLParser.class.getName());
+    private final String r2rmlPrefix = "rr";
+
     /**
      * Parses and returns the R2RML mapping configuration stored in
      * MappingDocument into R2RML containing the collection of mapping components
      * for each type of configuration.
+     * <p>
+     * The parser searches for triple maps by collecting subjects that have the
+     * logicalTable property since every triple maps must have a logicalTable property
+     * to be a valid map. From there, the parser will be able to search through the graph
+     * and gather the required properties and values to generate a R2RMLMap for
+     * the mapping document.
      *
-     * @param mappingDocument the document containing the mapping configuration
+     * @param mappingDoc the document containing the mapping configuration
      * @return the R2RMLMap containing the mapping components
      */
-    public R2RMLMap parse(MappingDocument mappingDocument) {
-        return new R2RMLMap();
+    public ConfigMap parse(MappingDocument mappingDoc) {
+        Model mappingGraph = mappingDoc.getMappingGraph();
+
+        String r2rmlPrefixURI = mappingGraph.getNsPrefixURI(r2rmlPrefix);
+        Property logicalTableProp = mappingGraph.getProperty(r2rmlPrefixURI, "logicalTable");
+        Property subjectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "subjectMap");
+        Property predicateObjectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "predicateObjectMap");
+        Property tableNameProp = mappingGraph.getProperty(r2rmlPrefixURI, "tableName");
+        Property templateProp = mappingGraph.getProperty(r2rmlPrefixURI, "template");
+        Property classProp = mappingGraph.getProperty(r2rmlPrefixURI, "class");
+        Property predicateProp = mappingGraph.getProperty(r2rmlPrefixURI, "predicate");
+        Property objectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "objectMap");
+        Property columnProp = mappingGraph.getProperty(r2rmlPrefixURI, "column");
+
+        ConfigMap configMap = ConfigMapFactory.createR2RMLMap();
+        ResIterator itr = mappingGraph.listSubjectsWithProperty(logicalTableProp);
+        while (itr.hasNext()) {
+            Resource res = itr.nextResource();
+
+            EntityMap triplesMap = new TriplesMap();
+            LOGGER.debug(res.getURI());
+
+            if (!res.hasProperty(subjectMapProp)) {
+                break;
+            }
+
+            //Check if any logical map exist
+            Resource res2 = res.getPropertyResourceValue(logicalTableProp);
+            LOGGER.debug(" : " + res2.getId());
+
+            //Check if logical table have table name
+            if (res2.hasProperty(tableNameProp)) {
+                Statement stmt = res2.getProperty(tableNameProp);
+                String tableName = stmt.getLiteral().toString();
+                LOGGER.debug("  : " + stmt.getPredicate() + " : " + tableName);
+                triplesMap.setEntitySource(tableName);
+            }
+
+            //Check if any subject map exist
+            Resource res3 = res.getPropertyResourceValue(subjectMapProp);
+            LOGGER.debug(" : " + res3.getId());
+
+            //Check if subject map have template
+            if (res3.hasProperty(templateProp)) {
+                Statement stmt = res3.getProperty(templateProp);
+                String template = stmt.getLiteral().toString();
+                LOGGER.debug("  : " + stmt.getPredicate() + " : " + template);
+                triplesMap.setTemplate(template);
+            }
+
+            //Check if subject map have class type
+            if (res3.hasProperty(classProp)) {
+                Statement stmt = res3.getProperty(classProp);
+                String classType = stmt.getResource().getURI();
+                LOGGER.debug("  : " + stmt.getPredicate() + " : " + classType);
+                triplesMap.setClassType(classType);
+            }
+
+            if (res.hasProperty(predicateObjectMapProp)) {
+                //Check if any predicate object map exists
+                Resource res4 = res.getPropertyResourceValue(predicateObjectMapProp);
+                LOGGER.debug(" : " + res4.getId());
+
+                PredicateObjectMap predicateObjectMap = new PredicateObjectMapCom();
+
+                //Check if predicate object map have predicate
+                if (res4.hasProperty(predicateProp)) {
+                    Statement stmt = res4.getProperty(predicateProp);
+                    String predicate = stmt.getResource().getURI();
+                    LOGGER.debug("  : " + stmt.getPredicate() + " : " + predicate);
+                    predicateObjectMap.setPredicate(predicate);
+                }
+
+                //Check if predicate object map have object map
+                Resource objectResource = res4.getPropertyResourceValue(objectMapProp);
+                LOGGER.debug("  : " + objectResource.getId());
+
+                if (objectResource.hasProperty(columnProp)) {
+                    Statement stmt = objectResource.getProperty(columnProp);
+                    String column = stmt.getLiteral().toString();
+                    LOGGER.debug("   : " + stmt.getPredicate() + " : " + column);
+                    predicateObjectMap.setObjectSource(column);
+                }
+                triplesMap.addPredicateObjectMap(predicateObjectMap);
+            }
+            configMap.addEntityMap(res.getLocalName(), triplesMap);
+        }
+
+        return configMap;
     }
+
 }
--- src/test/java/r2graph/r2rml/R2RMLParserTest.java
+++ src/test/java/r2graph/r2rml/R2RMLParserTest.java
@@ -1,39 +1,80 @@
 package r2graph.r2rml;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import r2graph.configmap.ConfigMap;
+import r2graph.configmap.EntityMap;
+import r2graph.configmap.PredicateObjectMap;
+import r2graph.configmap.impl.PredicateObjectMapCom;
 import r2graph.util.MappingDocument;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for R2RMLParser code. Currently skeletal till further
  * iteration of the project or feature.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class R2RMLParserTest {
 
+    private final String triplesMap = "TriplesMap1";
+    private final String entitySource = "EMP";
+    private final String classType = "http://example.com/ns#Employee";
+    private final String template = "http://data.example.com/employee/{EMPNO}";
+    private final String predicate = "http://example.com/ns#name";
+    private final String column = "ENAME";
     @Mock
     MappingDocument mappingDocument;
     private R2RMLParser r2rmlParser;
-    private R2RMLMap expectedResult;
+    @Mock
+    private TriplesMap expectedResult;
 
     @Before
     public void setUp() throws Exception {
         r2rmlParser = new R2RMLParser();
+        setupFakeTriplesMap();
+
+        Model r2rmlInput = ModelFactory.createDefaultModel();
+        r2rmlInput.read(getClass().getResourceAsStream("../../r2rml_input_tableName_singlePom_01.ttl"), "http://example.com/ns#", "TTL");
+        when(mappingDocument.getMappingGraph()).thenReturn(r2rmlInput);
+    }
+
+    private void setupFakeTriplesMap(){
+        expectedResult = new TriplesMap();
+        expectedResult.setEntitySource(entitySource);
+        expectedResult.setClassType(classType);
+        expectedResult.setTemplate(template);
+        PredicateObjectMap predicateObjectMap = new PredicateObjectMapCom();
+        predicateObjectMap.setPredicate(predicate);
+        predicateObjectMap.setObjectSource(column);
+        expectedResult.addPredicateObjectMap(predicateObjectMap);
     }
 
     /**
      * Tests that the R2RMLParser returns a {@code R2RMLMap} after parsing
-     * a given {@code MappingDocument}
+     * a given {@code MappingDocument}.
      */
     @Test
     public void WhenParseMappingDocument_ShouldReturnR2RMLMap() {
-
-
-        R2RMLMap result = r2rmlParser.parse(mappingDocument);
+        ConfigMap result = r2rmlParser.parse(mappingDocument);
         assertThat(result, is(notNullValue()));
+    }
+
+    /**
+     * Tests that the R2RMLParser returns a populated {@code R2RMLMap} from data
+     * in {@code MappingDocument}. Note: Only a shallow comparision atm.
+     */
+    @Test
+    public void WhenParseValidMappingDocument_ShouldReturnPopulatedR2RMLMap() {
+        EntityMap result = r2rmlParser.parse(mappingDocument).listEntityMaps().get(triplesMap);
+        assertThat(result, samePropertyValuesAs(expectedResult));
     }
 }
