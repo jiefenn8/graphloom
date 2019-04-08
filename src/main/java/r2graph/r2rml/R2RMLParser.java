4ad07e43eac38b8490365527e20eb71d762855cb
Javen Liu <jiefen8nz@gmail.com>
2019-04-08 23:44:41 +1200

    Refactor R2RML parser

--- src/main/java/r2graph/r2rml/R2RMLParser.java
+++ src/main/java/r2graph/r2rml/R2RMLParser.java
@@ -19,6 +19,8 @@ public class R2RMLParser {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(R2RMLParser.class.getName());
     private final String r2rmlPrefix = "rr";
+    private Model mappingGraph;
+    private String r2rmlPrefixURI;
 
     /**
      * Parses and returns the R2RML mapping configuration stored in
@@ -35,94 +37,93 @@ public class R2RMLParser {
      * @return the R2RMLMap containing the mapping components
      */
     public ConfigMap parse(MappingDocument mappingDoc) {
-        Model mappingGraph = mappingDoc.getMappingGraph();
+        setupParser(mappingDoc.getMappingGraph());
+        return findTriplesMaps();
+    }
 
-        String r2rmlPrefixURI = mappingGraph.getNsPrefixURI(r2rmlPrefix);
-        Property logicalTableProp = mappingGraph.getProperty(r2rmlPrefixURI, "logicalTable");
-        Property subjectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "subjectMap");
-        Property predicateObjectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "predicateObjectMap");
-        Property tableNameProp = mappingGraph.getProperty(r2rmlPrefixURI, "tableName");
-        Property templateProp = mappingGraph.getProperty(r2rmlPrefixURI, "template");
-        Property classProp = mappingGraph.getProperty(r2rmlPrefixURI, "class");
-        Property predicateProp = mappingGraph.getProperty(r2rmlPrefixURI, "predicate");
-        Property objectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "objectMap");
-        Property columnProp = mappingGraph.getProperty(r2rmlPrefixURI, "column");
+    private void setupParser(Model mappingGraph) {
+        this.mappingGraph = mappingGraph;
+        r2rmlPrefixURI = this.mappingGraph.getNsPrefixURI(r2rmlPrefix);
+    }
 
+    private ConfigMap findTriplesMaps() {
         ConfigMap configMap = ConfigMapFactory.createR2RMLMap();
-        ResIterator itr = mappingGraph.listSubjectsWithProperty(logicalTableProp);
+        ResIterator itr = mappingGraph.listSubjectsWithProperty(mappingGraph.getProperty(r2rmlPrefixURI, "logicalTable"));
         while (itr.hasNext()) {
             Resource res = itr.nextResource();
-
             EntityMap triplesMap = new TriplesMap();
-            LOGGER.debug(res.getURI());
 
-            if (!res.hasProperty(subjectMapProp)) {
-                break;
-            }
+            findLogicalTable(res, triplesMap);
+            findSubjectMap(res, triplesMap);
+            findPredicateObjectMaps(res, triplesMap);
 
-            //Check if any logical map exist
-            Resource res2 = res.getPropertyResourceValue(logicalTableProp);
-            LOGGER.debug(" : " + res2.getId());
-
-            //Check if logical table have table name
-            if (res2.hasProperty(tableNameProp)) {
-                Statement stmt = res2.getProperty(tableNameProp);
-                String tableName = stmt.getLiteral().toString();
-                LOGGER.debug("  : " + stmt.getPredicate() + " : " + tableName);
-                triplesMap.setEntitySource(tableName);
-            }
-
-            //Check if any subject map exist
-            Resource res3 = res.getPropertyResourceValue(subjectMapProp);
-            LOGGER.debug(" : " + res3.getId());
-
-            //Check if subject map have template
-            if (res3.hasProperty(templateProp)) {
-                Statement stmt = res3.getProperty(templateProp);
-                String template = stmt.getLiteral().toString();
-                LOGGER.debug("  : " + stmt.getPredicate() + " : " + template);
-                triplesMap.setTemplate(template);
-            }
-
-            //Check if subject map have class type
-            if (res3.hasProperty(classProp)) {
-                Statement stmt = res3.getProperty(classProp);
-                String classType = stmt.getResource().getURI();
-                LOGGER.debug("  : " + stmt.getPredicate() + " : " + classType);
-                triplesMap.setClassType(classType);
-            }
-
-            if (res.hasProperty(predicateObjectMapProp)) {
-                //Check if any predicate object map exists
-                Resource res4 = res.getPropertyResourceValue(predicateObjectMapProp);
-                LOGGER.debug(" : " + res4.getId());
-
-                PredicateObjectMap predicateObjectMap = new PredicateObjectMap();
-
-                //Check if predicate object map have predicate
-                if (res4.hasProperty(predicateProp)) {
-                    Statement stmt = res4.getProperty(predicateProp);
-                    String predicate = stmt.getResource().getURI();
-                    LOGGER.debug("  : " + stmt.getPredicate() + " : " + predicate);
-                    predicateObjectMap.setPredicate(predicate);
-                }
-
-                //Check if predicate object map have object map
-                Resource objectResource = res4.getPropertyResourceValue(objectMapProp);
-                LOGGER.debug("  : " + objectResource.getId());
-
-                if (objectResource.hasProperty(columnProp)) {
-                    Statement stmt = objectResource.getProperty(columnProp);
-                    String column = stmt.getLiteral().toString();
-                    LOGGER.debug("   : " + stmt.getPredicate() + " : " + column);
-                    predicateObjectMap.setObjectSource(column);
-                }
-                triplesMap.addPredicateMap(predicateObjectMap);
-            }
             configMap.addEntityMap(res.getLocalName(), triplesMap);
         }
-
         return configMap;
     }
 
+    private void findPredicateObjectMaps(Resource resource, EntityMap triplesMap) {
+        Property predicateObjectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "predicateObjectMap");
+        Property objectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "objectMap");
+        Property predicateProp = mappingGraph.getProperty(r2rmlPrefixURI, "predicate");
+        Property columnProp = mappingGraph.getProperty(r2rmlPrefixURI, "column");
+
+        if (resource.hasProperty(predicateObjectMapProp)) {
+            //Check if any predicate object map exists
+            Resource res4 = resource.getPropertyResourceValue(predicateObjectMapProp);
+            PredicateObjectMap predicateObjectMap = new PredicateObjectMap();
+
+            //Check if predicate object map have predicate
+            if (res4.hasProperty(predicateProp)) {
+                Statement stmt = res4.getProperty(predicateProp);
+                predicateObjectMap.setPredicate(stmt.getResource().getURI());
+            }
+
+            //Check if predicate object map have object map
+            Resource objectResource = res4.getPropertyResourceValue(objectMapProp);
+
+            if (objectResource.hasProperty(columnProp)) {
+                Statement stmt = objectResource.getProperty(columnProp);
+                predicateObjectMap.setObjectSource(stmt.getLiteral().toString());
+            }
+            triplesMap.addPredicateMap(predicateObjectMap);
+        }
+    }
+
+    private boolean findSubjectMap(Resource resource, EntityMap triplesMap) {
+        Property subjectMapProp = mappingGraph.getProperty(r2rmlPrefixURI, "subjectMap");
+        Property templateProp = mappingGraph.getProperty(r2rmlPrefixURI, "template");
+        Property classProp = mappingGraph.getProperty(r2rmlPrefixURI, "class");
+
+        //Check if any subject map exist
+        Resource subjectResource = resource.getPropertyResourceValue(subjectMapProp);
+
+        //Check if subject map have template
+        if (subjectResource.hasProperty(templateProp)) {
+            Statement stmt = subjectResource.getProperty(templateProp);
+            triplesMap.setTemplate(stmt.getLiteral().toString());
+        }
+
+        //Check if subject map have class type
+        if (subjectResource.hasProperty(classProp)) {
+            Statement stmt = subjectResource.getProperty(classProp);
+            triplesMap.setClassType(stmt.getResource().getURI());
+        }
+
+        return true;
+    }
+
+    private void findLogicalTable(Resource resource, EntityMap triplesMap) {
+        Property tableNameProp = mappingGraph.getProperty(r2rmlPrefixURI, "tableName");
+
+        //Check if any logical map exist
+        Resource logicalTableResource = resource.getPropertyResourceValue(
+                mappingGraph.getProperty(r2rmlPrefixURI, "logicalTable"));
+
+        //Check if logical table have table name
+        if (logicalTableResource.hasProperty(tableNameProp)) {
+            Statement stmt = logicalTableResource.getProperty(tableNameProp);
+            triplesMap.setEntitySource(stmt.getLiteral().toString());
+        }
+    }
 }
