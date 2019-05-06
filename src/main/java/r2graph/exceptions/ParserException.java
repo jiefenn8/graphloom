88113bc514d4e3533087bfb8a2c95c31514717f9
jiefenn8 <jiefen8nz@gmail.com>
2019-05-07 03:13:23 +1200

    Add exception classes

---
+++ src/main/java/r2graph/exceptions/ParserException.java
@@ -0,0 +1,7 @@
+package r2graph.exception;
+
+/**
+ * Exception to throw for parser operation.
+ */
+public class ParserException extends R2GraphException {
+}
---
+++ src/main/java/r2graph/exceptions/base/FeijoaException.java
@@ -0,0 +1,11 @@
+package r2graph.exceptions;
+
+/**
+ * Superclass of exceptions happening from R2Graph code.
+ */
+public class FeijoaException extends RuntimeException {
+
+    public FeijoaException(){super();}
+    public FeijoaException(String message){super(message);}
+
+}
