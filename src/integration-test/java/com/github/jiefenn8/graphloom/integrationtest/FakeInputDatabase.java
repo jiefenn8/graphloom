0f13e364349af12a43e4a5eab9bccdff5600804c
jiefenn8 <jiefenn8@outlook.com>
2021-02-04 23:55:59 +1300

    Add new InputSource implementation
    Add new InputSource implementation that will replace the previous
    placeholder implementation and the need of in memory copy of input data.
    This new implementation should stream the data directly from the
    data source handler to the mapper. (Depending on implementation of the
    adapters)

--- src/integration-test/java/com/github/jiefenn8/graphloom/integrationtest/FakeInputDatabase.java
+++ src/integration-test/java/com/github/jiefenn8/graphloom/integrationtest/FakeInputDatabase.java
@@ -6,7 +6,16 @@
 package com.github.jiefenn8.graphloom.integrationtest;
 
 import com.github.jiefenn8.graphloom.api.*;
+import com.github.jiefenn8.graphloom.api.inputsource.Entity;
+import com.github.jiefenn8.graphloom.api.inputsource.EntityResult;
+import com.github.jiefenn8.graphloom.integrationtest.fake.FakeEntity;
+import com.github.jiefenn8.graphloom.integrationtest.fake.FakeEntityResult;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * Fake class simulating InputSource implementation of a database.
@@ -62,5 +71,39 @@ public class FakeInputDatabase implements InputSource {
     @Override
     public int calculateNumOfBatches(EntityReference sourceConfig) {
         return 1;
+    }
+
+    private EntityResult getEMPEntityResults() {
+        Map<String, String> record = ImmutableMap.of(
+                "EMPNO", "7369",
+                "ENAME", "SMITH",
+                "JOB", "CLERK",
+                "DEPTNO", "10"
+        );
+        Entity fakeEntity = new FakeEntity(record);
+        return new FakeEntityResult(ImmutableSet.of(fakeEntity));
+    }
+
+    private EntityResult getDEPTEntityResults() {
+        Map<String, String> record = ImmutableMap.of(
+                "DEPTNO", "10",
+                "ENAME", "SMITH",
+                "JOB", "CLERK",
+                "DEPTNO", "10"
+        );
+        Entity fakeEntity = new FakeEntity(record);
+        return new FakeEntityResult(ImmutableSet.of(fakeEntity));
+    }
+
+    @Override
+    public void executeEntityQuery(EntityReference entityRef, Consumer<EntityResult> action) {
+        String payload = entityRef.getPayload();
+        if (payload.equals("DEPT")) {
+            action.accept(getDEPTEntityResults());
+        } else if (payload.equals("EMP")) {
+            action.accept(getEMPEntityResults());
+        } else {
+            action.accept(new FakeEntityResult(new HashSet<>()));
+        }
     }
 }
--- src/main/java/com/github/jiefenn8/graphloom/api/InputSource.java
+++ src/main/java/com/github/jiefenn8/graphloom/api/InputSource.java
@@ -5,9 +5,13 @@
 
 package com.github.jiefenn8.graphloom.api;
 
+import com.github.jiefenn8.graphloom.api.inputsource.EntityResult;
+
+import java.util.function.Consumer;
+
 /**
- * This interface defines the base methods to retrieve data of entities as
- * a entity record from a data source.
+ * This interface defines the base methods in retrieving relevant data grouped
+ * through a defined entity; usually by name reference or a query.
  */
 public interface InputSource {
 
@@ -34,4 +38,13 @@ public interface InputSource {
      * @return the number of batches possible
      */
     int calculateNumOfBatches(EntityReference sourceConfig);
+
+    /**
+     * Execute the reference or query in {@link EntityReference} to obtain
+     * relevant data as {@link EntityResult} and apply any action to it.
+     *
+     * @param entityRef the reference or query to retrieve relevant data
+     * @param action    the action to apply to the results
+     */
+    void executeEntityQuery(EntityReference entityRef, Consumer<EntityResult> action);
 }
